0efa683a3b967f1a43129e0c50a3201c2988922b
croxis <croxis@yahoo.com>
2012-07-31 14:45:15 -0700

    Test to try redstone elevators

--- src/main/java/net/croxis/plugins/lift/LiftRedstoneListener.java
+++ src/main/java/net/croxis/plugins/lift/LiftRedstoneListener.java
@@ -34,7 +34,7 @@ public class LiftRedstoneListener implements Listener {
 		Block block = event.getBlock();
 		Elevator elevator = null;
 		if ((block.getType() == Material.STONE_BUTTON) 
-				&& (!block.isBlockIndirectlyPowered())
+				//&& (!block.isBlockIndirectlyPowered())
 				&& block.getRelative(BlockFace.UP).getType() == Material.WALL_SIGN){
 			long startTime = System.currentTimeMillis();
 			elevator = new Elevator(this.plugin, block);
