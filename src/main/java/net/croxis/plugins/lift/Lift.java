3d1a13c62267e977f76f2cc427450e0ee2060213
croxis <croxis@yahoo.com>
2012-09-04 15:01:26 -0700

    Fix

--- src/main/java/net/croxis/plugins/lift/Lift.java
+++ src/main/java/net/croxis/plugins/lift/Lift.java
@@ -19,7 +19,6 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.*;
-import org.mcstats.Metrics;
 
 public class Lift extends JavaPlugin implements Listener {
 	public static boolean debug = false;
