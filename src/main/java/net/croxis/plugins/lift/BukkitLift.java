b3fe9c74f8661179b5f5381db691d3a516f1a045
croxis <croxis@yahoo.com>
2013-08-05 14:45:19 -0700

    Updated version

--- src/main/java/net/croxis/plugins/lift/BukkitLift.java
+++ src/main/java/net/croxis/plugins/lift/BukkitLift.java
@@ -34,7 +34,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 import org.bukkit.command.*;
 
 public class BukkitLift extends JavaPlugin implements Listener {
